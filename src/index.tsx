adfc0697e6c3fa0b569d0a101be9418ccca64f21
Luke Askins <lukeaskins@gmail.com>
2020-12-01 21:33:01 -0600

    Updates dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import theme from './theme';
 import App from './App';
 import { TicTacToeDataProvider, TTTSTtate } from './TicTacToeContext';
 import './index.css';
